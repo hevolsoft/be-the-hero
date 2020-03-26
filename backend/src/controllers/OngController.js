c6a8d9a8a184488672f47840b3c47a4371ed1c4b
caemat <caemat_gamer@hotmail.com>
2020-03-25 23:33:47 -0300

    Front e back

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,27 +1,27 @@
-const crypto = require('crypto');
-const connection = require('../database/connection');
+const connection = require("../database/connection");
+const crypto = require("crypto");
 
 module.exports = {
-    async index (request, response)  {
-        const ongs = await connection('ongs').select('*');
-    
-        return response.json(ongs);
-    },
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
 
-    async create(request, response){
-        const { name, email, whatsapp, city, uf} = request.body;
-    
-   const id = crypto.randomBytes(4).toString('HEX');
+    return res.json(ongs);
+  },
 
-   await connection('ongs').insert({
-     id,
-     name,
-     email,
-     whatsapp,
-     city,
-     uf,  
-   })
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
 
-        return response.json({ id });
-    }
+    const id = crypto.randomBytes(4).toString("HEX");
+
+    await connection("ongs").insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return res.json({ id });
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
-
-
 app.listen(3333);
-/* Metodos HTTP
-GET: Buscar/listar iformação no backend
-POST: criar uma informação no backend
-PUT: alterar uma informação no backend
-DELETE: deletar informação no backend
-*/
-/*
-Tipos de parametros
-Query: Parametros nomeados na rota apos o "?"(Filtros,Paginação)
-Route: Parametros utilizados para identificar rescursos
-Request bory:
-*/
-/* 
-SQL: MySql, SQlite, postgreSQL, oracle
-NoSQL: MongoDB, CouchDB, etc
-*/
-/*
-Driver: SELECT * FROM users
-Query Builder: table('users).select('*).where()
-*/
